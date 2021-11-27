836f156e831ba0cb12daa1a9eb794a4cf6e5ed8c
Robert Klinger <robert@k1inger.de>
2021-11-27 21:11:15 +0100

    added model dumps

---
+++ src/models/trained_model_dumps/tpot_binary_pipeline.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:c022dcb04fc5df98703793fed561296f7152a51eea38d4cc3f38913081d4ec42
+size 1167
---
+++ src/models/trained_model_dumps/tpot_pipeline.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:5e5c6ca9f2d09fe76abf72c97e344a2124c4378cd57122fb8690edb29d0efbc6
+size 1241
